d90f4bc1b2c32af3426c79de1c372da07f9fba91
jrcardarelli <jrcardarelli@wpi.edu>
2023-01-13 14:37:12 -0500

    added branch function

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Connor");
-    System.out.print("Joseph");
+    System.out.println("Joseph");
     App.launch(App.class, args);
     System.out.println("Lindsey");
+  }
+
+  public void branch()
+  {
+    System.out.println("Branch - Joseoh");
   }
 }

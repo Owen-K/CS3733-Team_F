193af70f4ef9deac71114ee34c1139c99ad1ecf4
Ian W <49083526+Landaman@users.noreply.github.com>
2023-01-13 14:38:25 -0500

    Print Ian Branch Function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ian");
     }
+
+    public static void printIanBranch() {
+        System.out.println("Branch - Ian");
+    }
 }
